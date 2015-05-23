31703f7cc43bced3f9f30d4aea81fca57b785378
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-05-23 15:05:16 +0530

    Add the /home route to the application
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'pages/index'
 
   get 'users/index'
 
   get 'users/show'
 
   devise_for :users
+
+  get '/home' => 'pages#index'
+
+  root 'routing#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
