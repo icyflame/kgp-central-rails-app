6a58920e3a8abde22a0258d6466526c537e94e8c
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-05-24 14:22:32 +0530

    Add the store action in the advt controller

--- app/controllers/advertisement_controller.rb
+++ app/controllers/advertisement_controller.rb
@@ -1,4 +1,5 @@
 class AdvertisementController < ApplicationController
+	before_action :authenticate_user!
   def post
   end
 
@@ -6,5 +7,33 @@ class AdvertisementController < ApplicationController
   end
 
   def viewall
+  end
+
+  def store
+  	'''
+  	The posted parameters of the Ad will be recieved
+  	by this function.
+  	'''
+  	new_ad = Advt.new
+
+  	user_id = current_user.id
+
+  	new_ad.user_id = user_id
+  	new_ad.title = params[:title]
+  	new_ad.description = params[:description]
+  	new_ad.category = params[:category]
+
+  	if params[:negotiable].to_s == "no"
+  		new_ad.negotiable = false
+  	end
+
+  	# render plain: new_ad.inspect
+
+  	new_ad.save
+
+  	render plain: "Saved!"
+
+  	# render plain: params.inspect
+
   end
 end
